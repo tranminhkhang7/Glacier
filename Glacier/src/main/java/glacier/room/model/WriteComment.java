89f3f3ed9567174ce1987134ee9f1df24136558c
Sol-Tran <khaitqse161901@fpt.edu.vn>
2022-06-01 11:27:31 +0700

    DONT PULL THIS COMMIT TO MAIN, THIS IS JUST TEST CODE

---
+++ Glacier/src/main/java/glacier/room/model/WriteComment.java
@@ -0,0 +1,98 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+package glacier.room.model;
+
+import glacier.room.dbmanager.CommentManager;
+import glacier.user.model.Account;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Timestamp;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ *
+ * @author Admin
+ */
+@WebServlet(name = "WriteComment", urlPatterns = {"/WriteComment"})
+public class WriteComment extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try{
+            HttpSession session = request.getSession(false);
+            Account a = (Account)session.getAttribute("USER_DATA");
+            a = new Account("hehe@gmail.com","12345678","tenant");
+            if (a.getRole().equals("tenant")){
+                CommentManager cm = new CommentManager();
+                int commentID = cm.getNextCommentIndex();
+                long now = System.currentTimeMillis();
+                Timestamp date = new Timestamp(now);
+                int roomID = Integer.parseInt(request.getParameter("roomID"));
+                int rating = Integer.parseInt(request.getParameter("rating"));
+                String content = request.getParameter("review");
+                
+                Comment c = new Comment(commentID,a.getEmail(),roomID,content,date);
+            }
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
