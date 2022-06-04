3718a7111df4e58b0645cac7a244072caf4f2a3a
Sol-Tran <khaitqse161901@fpt.edu.vn>
2022-06-04 13:15:04 +0700

    Khai's code checkpoint

---
+++ Glacier/src/main/java/glacier/room/model/Comment.java
@@ -0,0 +1,113 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package glacier.room.model;
+
+import glacier.user.model.Tenant;
+import java.sql.Timestamp;
+import java.util.Date;
+
+/**
+ *
+ * @author Admin
+ */
+public class Comment {
+    private int id;
+    private String name;
+    private String profile_picture;
+    private int roomId;
+    private String content;
+    private Date time;
+    private int rating;
+    private String email;
+    private Timestamp date;
+
+    public Comment() {
+    }
+
+    public Comment(int id, String name, String profile_picture, int roomId, String content, Date time, int rating) {
+        this.id = id;
+        this.name = name;
+        this.profile_picture = profile_picture;
+        this.roomId = roomId;
+        this.content = content;
+        this.time = time;
+        this.rating = rating;
+    }
+
+    public Comment(int id, int roomId, String content, int rating, String email, Timestamp date) {
+        this.id = id;
+        this.roomId = roomId;
+        this.content = content;
+        this.rating = rating;
+        this.email = email;
+        this.date = date;
+    }
+
+    Comment(int commentID, String email, int roomID, String content, Timestamp date) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getProfile_picture() {
+        return profile_picture;
+    }
+
+    public void setProfile_picture(String profile_picture) {
+        this.profile_picture = profile_picture;
+    }
+
+    public int getRoomId() {
+        return roomId;
+    }
+
+    public void setRoomId(int roomId) {
+        this.roomId = roomId;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Date getTime() {
+        return time;
+    }
+
+    public void setTime(Date time) {
+        this.time = time;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+
+    @Override
+    public String toString() {
+        return "Comment{" + "id=" + id + ", name=" + name + ", profile_picture=" + profile_picture + ", roomId=" + roomId + ", content=" + content + ", time=" + time + ", rating=" + rating + '}';
+    }
+
+}
