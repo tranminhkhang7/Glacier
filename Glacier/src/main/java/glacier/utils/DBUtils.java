3d207e10d743c53f5463405d84e325213031369c
Sol-Tran <101551006+Sol-Tran@users.noreply.github.com>
2022-06-17 00:13:49 +0700

    Update DBUtils.java

--- Glacier/src/main/java/glacier/utils/DBUtils.java
+++ Glacier/src/main/java/glacier/utils/DBUtils.java
@@ -21,7 +21,7 @@ public class DBUtils {
     private final static String instance = "";//LEAVE THIS ONE EMPTY IF YOUR SQL IS A SINGLE INSTANCE
     private final static String userID = "sa";
 
-    private final static String password = "khangtm";
+    private final static String password = "123456789";
 
 
 public static Connection getConnection() throws Exception {
